19e28e4feae14962e49370763e8654220c537360
Glowman554 <Glowman434@gmail.com>
2022-10-08 07:56:54 +0000

    use 3 buttons to input text in the lcd

--- main.cpp
+++ main.cpp
@@ -6,16 +6,37 @@
 #include "mbed.h"
 #include <lcd.hpp>
 
-DigitalOut led(LED1);
+DigitalIn up_button(PB_0, PullDown);
+DigitalIn down_button(PB_3, PullDown);
+DigitalIn ok_button(PB_4, PullDown);
+
+char input[] = "                ";
+int idx = 0;
+
+void update(software_lcd& lcd) {
+    lcd.clear();
+
+    for (int i = 0; i < idx + 1; i++) {
+        lcd.putc(input[i]);
+    }
+}
 
 int main() {
     software_lcd lcd;
-    lcd.clear();
-    lcd.puts("Hello world");
-    lcd.set_flags(LCD_CURSOR | LCD_CURSOR_BLINK);
 
     while (true) {
+        if (up_button) {
+            input[idx]++;
+        } else if(down_button) {
+            input[idx]--;
+        } else if (ok_button) {
+            idx++;
+        } else {
+            goto skip_sleep_and_update;
+        }
+
+        update(lcd);
         ThisThread::sleep_for(200ms);
-        led = !led;
+    skip_sleep_and_update:;
     }
 }
