26605d5e454d793b544267545c91d9efc2b45dfd
Glowman554 <Glowman434@gmail.com>
2022-10-08 13:31:00 +0000

    simple calculator

--- main.cpp
+++ main.cpp
@@ -6,23 +6,25 @@
 #include "mbed.h"
 #include <lcd.hpp>
 
+extern "C" {
+    #include <calc.h>
+}
+
+#define SLEEP_TIME 100ms
+
 DigitalIn up_button(PB_0, PullDown);
 DigitalIn down_button(PB_3, PullDown);
 DigitalIn ok_button(PB_4, PullDown);
 
-char input[] = "                ";
-int idx = 0;
-
-void update(software_lcd& lcd) {
-    lcd.clear();
-
-    for (int i = 0; i < idx + 1; i++) {
-        lcd.putc(input[i]);
-    }
-}
-
 int main() {
     software_lcd lcd;
+    lcd.set_flags(LCD_CURSOR);
+
+    
+    char input[32];
+    memset(input, ' ', sizeof(input));
+    int idx = 0;
+
 
     while (true) {
         if (up_button) {
@@ -30,13 +32,33 @@ int main() {
         } else if(down_button) {
             input[idx]--;
         } else if (ok_button) {
-            idx++;
+            if (input[idx] == '=') {
+                input[idx] = 0;
+                
+                char buf[16] = { 0 };
+                sprintf(buf, "= %d", calc(input));
+                lcd.puts(buf);
+
+                idx = 0;
+                memset(input, ' ', sizeof(input));
+    
+                ThisThread::sleep_for(SLEEP_TIME);
+                goto skip_sleep_and_update;
+            } else {
+                idx++;
+            }
         } else {
             goto skip_sleep_and_update;
         }
 
-        update(lcd);
-        ThisThread::sleep_for(200ms);
+        lcd.clear();
+
+        for (int i = 0; i < idx + 1; i++) {
+            lcd.putc(input[i]);
+        }
+        lcd.cursor(idx);
+        
+        ThisThread::sleep_for(SLEEP_TIME);
     skip_sleep_and_update:;
     }
 }
